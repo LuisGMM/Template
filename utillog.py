f7c044156eeba6308fd3f5d0818887fcb9099194
LuisGMM <emaildemiscuentas@gmail.com>
2022-04-27 21:00:06 +0200

    Update utillog.py

--- utillog.py
+++ utillog.py
@@ -1,3 +1,10 @@
 import logging
 
 logging.basicConfig(level=logging.INFO, filename='log.log', filemode='w', format='%(asctime)s - %(levelname) - %(message)s')
+
+logging.info('info')
+logging.debug('debug')
+logging.warning('warning')
+logging.error('error')
+logging.critical('critical')
+
